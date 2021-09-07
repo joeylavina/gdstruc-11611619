3c2bcfb21e60367f0a235e1d5743eec9875d7d1c
Joey Laviña <josegabriel.lavina@benilde.edu.ph>
2021-09-07 12:47:27 +0800

    Added Sample Project

---
+++ Sample Project/src/com/JoseGabriel/Main.java
@@ -0,0 +1,22 @@
+package com.JoseGabriel;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+
+        //Gets user input
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
